a4afd6f0fd0272027797f654a3dbaf397b36f81a
Vitalii <vtysiachnyk@gmail.com>
2026-06-21 20:01:00 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -358,3 +358,43 @@ const people = [
 console.log(people); // you can remove it
 
 // write your code here
+const formattedPeople = people.map((person) => {
+  const age = person.died - person.born;
+  const sex = person.sex === 'm' ? 'Male' : 'Female';
+  const century = Math.ceil(person.died / 100);
+
+  return {
+    ...person,
+    sex,
+    age,
+    century,
+  };
+});
+
+const table = document.querySelector('.dashboard');
+
+formattedPeople.forEach((person) => {
+  const tr = document.createElement('tr');
+  const tdName = document.createElement('td');
+  const tdSex = document.createElement('td');
+  const tdBorn = document.createElement('td');
+  const tdDied = document.createElement('td');
+  const tdAge = document.createElement('td');
+  const tdCentury = document.createElement('td');
+
+  tdName.innerHTML = person.name;
+  tdSex.innerHTML = person.sex;
+  tdBorn.innerHTML = person.born;
+  tdDied.innerHTML = person.died;
+  tdAge.innerHTML = person.age;
+  tdCentury.innerHTML = person.century;
+
+  tr.append(tdName);
+  tr.append(tdSex);
+  tr.append(tdBorn);
+  tr.append(tdDied);
+  tr.append(tdAge);
+  tr.append(tdCentury);
+
+  table.append(tr);
+});
